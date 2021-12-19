b6bb70c895183c28f6973ae44c8f2b776969e899
Muntasir <muntasirenamalhuda@gmail.com>
2021-12-19 21:10:07 +0000

    added jump functionality

--- scripts/dino.js
+++ scripts/dino.js
@@ -1,23 +1,36 @@
+import {
+	incrementCustomProperty,
+	setCustomProperty,
+	getCustomProperty,
+} from './updateCustomProperty.js';
+
 const dino = document.querySelector('[data-dino]');
 
 const JUMP_SPEED = 0.45;
-const GRAVITY = 0.011;
+const GRAVITY = 0.0015;
 const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100; //every single frame of dino should last for 100 ms
 
 let isJumping;
 let currentFrameTime;
 let dinoFrame;
+let yVelocity;
 
 export function setupDino() {
 	isJumping = false;
 	currentFrameTime = 0;
 	dinoFrame = 0;
+	yVelocity = 0;
+
+	setCustomProperty(dino, '--bottom', 0);
+
+	document.removeEventListener('keydown', onJump);
+	document.addEventListener('keydown', onJump);
 }
 
 export function updateDino(delta, speedScale) {
 	handleRun(delta, speedScale);
-	handleJump();
+	handleJump(delta);
 }
 
 /* for moving through different animations */
@@ -41,4 +54,22 @@ function handleRun(delta, speedScale) {
 
 function handleJump(delta) {
 	if (!isJumping) return;
+
+	incrementCustomProperty(dino, '--bottom', yVelocity * delta);
+
+	if (getCustomProperty(dino, '--bottom') <= 0) {
+		setCustomProperty(dino, '--bottom', 0);
+		isJumping = false;
+	}
+
+	yVelocity -= GRAVITY * delta;
+}
+
+function onJump(e) {
+	if (e.code !== 'Space' || isJumping) return;
+
+	yVelocity = JUMP_SPEED;
+	isJumping = true;
+
+	console.log('working');
 }
